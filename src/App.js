92e4d21cddaf320e28a7a103f07cb3bc27689313
tubi55 <tubi55@nate.com>
2022-02-07 11:46:05 +0900

    기본 라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,39 +1,36 @@
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
-
 import Visual from './components/main/Visual';
 import Intro from './components/main/Intro';
 import Info from './components/main/Info';
-
 import Department from './components/sub/Department';
 import Community from './components/sub/Community';
 import Gallery from './components/sub/Gallery';
 import Youtube from './components/sub/Youtube';
 import Location from './components/sub/Location';
 import Join from './components/sub/Join';
-
 import './scss/style.scss';
+import {Route} from 'react-router-dom';
 
-
-function App() {
+export default function App() {
   return (
     <div className="App">
       <Header />
 
-      <Visual />
-      <Intro />
-      <Info />
+      <Route exact path='/'>
+        <Visual />
+        <Intro />
+        <Info />
+      </Route>      
 
-      <Department />
-      <Community />
-      <Gallery />
-      <Youtube />
-      <Location />
-      <Join />
+      <Route path='/department' component={Department}></Route>
+      <Route path='/community' component={Community}></Route>
+      <Route path='/gallery' component={Gallery}></Route>
+      <Route path='/youtube' component={Youtube}></Route>
+      <Route path='/location' component={Location}></Route>
+      <Route path='/join' component={Join}></Route>     
 
       <Footer />
     </div>
   );
 }
-
-export default App;
