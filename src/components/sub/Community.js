c00004a0d9684053c59f4d2f038d6a850bb37bc4
tubi55 <tubi55@nate.com>
2022-02-07 16:41:33 +0900

    axios로 비동기 데이터 호출

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -4,22 +4,10 @@ export default function Community(){
   let main = useRef(null);
   let [index, setIndex] = useState(0);
 
-  useEffect(()=>{
-    //해당 컴포넌트가 생성될때 실행
-    console.log('Community 컴포넌트 생성');
-    main.current.classList.add('on');
-
-    //해당 컴포넌트가 소멸될때 실행
-    return ()=>{
-      console.log('Community 컴포넌트 소멸');    
-    }
+  useEffect(()=>{    
+    main.current.classList.add('on');    
   },[])
-
-  //index state값이 변경될때마사 실행
-  useEffect(()=>{
-    console.log('index값 변경됨');
-  },[index])
-
+  
   return (
     <main className="content community" ref={main}>
       <figure></figure>
@@ -36,10 +24,3 @@ export default function Community(){
     </main>
   )
 }
-/*
-  useEffect : 해당 컴포넌트의 생성, 상태값변경, 소멸이라는 생명주기에 따라 특정 구문을 실행할수 있는 hook 
-  -- useEffect 첫번째 인수로 콜백함수 등록
-  -- useEffect 두번째 인수로는 의존성을 등록 (원하는 state를 등록 가능)
-  -- useEffect 두번째 인수로 빈 배열을 의존성으로 등록 : 해당 컴포넌트 처음 생성될때 한번만 호출 가능
-  -- useEffect 콜백함수 안쪽에서 다시 return으로 함수를 내보내면 해당 함수의 구문은 컴포넌트가 소멸될때 호출됨
-*/
--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -1,10 +1,26 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import axios from 'axios';
 
 export default function Department(){
   let main = useRef(null);
+  //axios로 불러온 배열을 담을 빈배열을 state로 초기화
+  const [members, setMembers] = useState([]);
+  //public폴더의 절대값 경로 구함
+  const path = process.env.PUBLIC_URL;
+  //절대 경로에서부터의 json데이터 url값 구함
+  const url = `${path}/db/department.json`;
 
   useEffect(()=>{  
     main.current.classList.add('on');
+
+    //처음 컴포넌트 생성시
+    axios
+      .get(url)//비동적기적으로 데이터 호출
+      .then(json=>{ 
+        console.log(json.data.data);
+        //전달받은 데이터를 members state에 옮겨담음
+        setMembers(json.data.data);
+      });
   },[]);
 
   return (
@@ -13,7 +29,9 @@ export default function Department(){
       
       <div className="inner">
         <h1>Department</h1>
-        <section></section>
+        <section>
+
+        </section>
       </div>
     </main>
   )
