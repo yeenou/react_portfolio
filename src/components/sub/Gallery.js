da5bfd50dcd34fa1e2bcb1b9ec5c5b99b7d512ca
tubi55 <tubi55@nate.com>
2022-02-08 12:12:21 +0900

    레이어팝업 생성시 스크롤 기능 제거

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -8,7 +8,7 @@ export default function Gallery(){
   const [index, setIndex] = useState(0);
   const api_key = '89aae050d1d8c006bdb5bf866029199d';
   const method1 = 'flickr.interestingness.getList';
-  const num = 5;
+  const num = 20;
   const url = `https://www.flickr.com/services/rest/?method=${method1}&per_page=${num}&api_key=${api_key}&format=json&nojsoncallback=1`;
 
   useEffect(()=>{
@@ -32,7 +32,7 @@ export default function Gallery(){
             return (
               <article key={idx}>
                 <div className="inner">
-                  <div className="pic" onClick={()=>{
+                  <div className="pic" data={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`} onClick={()=>{
                     setIsPop(true);
                     setIndex(idx);
                   }}>
@@ -53,6 +53,11 @@ export default function Gallery(){
   )
 
   function Popup(){
+    useEffect(()=>{
+      document.body.style.overflow = 'hidden';
+      return ()=> document.body.style.overflow = 'auto';     
+    },[])
+
     return (
       <aside className="popup">
         <img src={`https://live.staticflickr.com/${items[index].server}/${items[index].id}_${items[index].secret}_b.jpg`} />
