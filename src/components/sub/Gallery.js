76b771bfd0a4833a4fba3c8450281cba154094ab
tubi55 <tubi55@nate.com>
2022-02-08 10:49:13 +0900

    flickr 이미지 갤러리 생성

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -1,10 +1,21 @@
-import { useEffect, useRef } from "react";
+import axios from 'axios';
+import { useEffect, useRef, useState } from "react";
 
 export default function Gallery(){ 
   let main = useRef(null);
+  let [items, setItems] = useState([]);
+  const api_key = '89aae050d1d8c006bdb5bf866029199d';
+  const method1 = 'flickr.interestingness.getList';
+  const num = 5;
+  const url = `https://www.flickr.com/services/rest/?method=${method1}&per_page=${num}&api_key=${api_key}&format=json&nojsoncallback=1`;
 
   useEffect(()=>{
     main.current.classList.add('on');
+
+    axios.get(url).then(json=>{
+      console.log(json.data.photos.photo);
+      setItems(json.data.photos.photo);
+    })
   },[]);
 
   return (
@@ -13,7 +24,21 @@ export default function Gallery(){
       
       <div className="inner">
         <h1>Gallery</h1>
-        <section></section>
+        <section>
+          {items.map((item,idx)=>{
+            return (
+              <article key={idx}>
+                <div className="inner">
+                  <div className="pic">
+                    <img src={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`} />
+                  </div>
+
+                  <h2>{item.title}</h2>
+                </div>
+              </article>
+            )            
+          })}
+        </section>
       </div>
     </main>
   )
