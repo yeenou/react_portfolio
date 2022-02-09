d16bdb47f9dcd7f7919fce9ba19549737cf90e4f
tubi55 <tubi55@nate.com>
2022-02-09 10:11:02 +0900

    교통정보 보기 이벤트 연결

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -1,9 +1,10 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Location(){
   let main = useRef(null);
   const {kakao} = window;
   const container = useRef(null);  
+  const [map, setMap] = useState(null);
 
   useEffect(()=>{
     main.current.classList.add('on');  
@@ -12,7 +13,9 @@ export default function Location(){
       center: new kakao.maps.LatLng(35.1529763,129.1245155),
       level: 3
     }
-    new kakao.maps.Map(container.current, options);
+    //카카오 api를 통해 리턴한 인스턴스를 state map에 옮겨담음
+    const map = new kakao.maps.Map(container.current, options);
+    setMap(map);
   },[]);
 
   return (
@@ -23,6 +26,14 @@ export default function Location(){
         <h1>Location</h1>
         <section>
           <div id="map" ref={container}></div>
+
+          <button onClick={()=>{
+            map.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+          }}>교통정보 보기</button>
+
+          <button onClick={()=>{
+            map.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+          }}>교통정보 끄기</button>
         </section>
       </div>
     </main>
