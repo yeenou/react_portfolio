56fd5b043ae97d129818df578a78f06b49f8f9f2
tubi55 <tubi55@nate.com>
2022-02-09 09:34:53 +0900

    카카오 api연결 및 지도 인스턴스 생성

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -2,9 +2,17 @@ import { useEffect, useRef } from "react";
 
 export default function Location(){
   let main = useRef(null);
+  const {kakao} = window;
+  const container = useRef(null);
 
   useEffect(()=>{
     main.current.classList.add('on');
+
+    const options = {
+      center: new kakao.maps.LatLng(),
+      level: 3
+    }
+    new kakao.map.Map(container.current, )
   },[]);
 
   return (
@@ -13,7 +21,9 @@ export default function Location(){
       
       <div className="inner">
         <h1>Location</h1>
-        <section></section>
+        <section>
+          <div id="map" ref={container}></div>
+        </section>
       </div>
     </main>
   )
