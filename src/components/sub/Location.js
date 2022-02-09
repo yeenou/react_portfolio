aa2bd94eaf586ce3e4949c94d0309088c2edc35e
tubi55 <tubi55@nate.com>
2022-02-09 09:48:09 +0900

    위도 경도 입력

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -3,16 +3,16 @@ import { useEffect, useRef } from "react";
 export default function Location(){
   let main = useRef(null);
   const {kakao} = window;
-  const container = useRef(null);
+  const container = useRef(null);  
 
   useEffect(()=>{
-    main.current.classList.add('on');
+    main.current.classList.add('on');  
 
     const options = {
-      center: new kakao.maps.LatLng(),
+      center: new kakao.maps.LatLng(35.1529763,129.1245155),
       level: 3
     }
-    new kakao.map.Map(container.current, )
+    new kakao.maps.Map(container.current, options);
   },[]);
 
   return (
