8f5f2256e0b58fb04df2f7338409948b437e495a
tubi55 <tubi55@nate.com>
2022-02-11 16:14:04 +0900

    로컬스토리지의 데이터를 메인페이지에 최신글 형식으로 호출

--- src/components/main/News.js
+++ src/components/main/News.js
@@ -1,10 +1,33 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 export default function News(){
+  const getLocalItems = () => {
+    let data = localStorage.getItem('posts');
+
+    if(data){
+      return JSON.parse(data);
+    }else{
+      return [];
+    }
+  }
+
+  const [posts] = useState(getLocalItems);
+
   return (
     <section id='news'>
       <div className="inner">
         <h1>Recent Post</h1>
+        
+        <ul>
+          {posts.map((post, idx)=>{
+            return (
+              <li key={idx}>
+                <h2>{post.title}</h2>
+                <p>{post.content}</p>
+              </li>
+            )
+          })}
+        </ul>
       </div>
     </section>
   )
