8008002e71c6ed2245db404a22da513366923606
tubi55 <tubi55@nate.com>
2022-02-11 16:47:32 +0900

    처음 메인페이지 접속시 기본데이터를 게시글로 출력하고 localStroage에 저장

--- src/components/main/News.js
+++ src/components/main/News.js
@@ -1,17 +1,32 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function News(){
+  //초기 로딩시 사용자 컴퓨터에 localStorage에 데이터가 없을시 임의로 보여줄 초기 데이터
+  const defaultData = [
+    {title: 'Hello1', content: 'Here comes description in detail.'},
+    {title: 'Hello2', content: 'Here comes description in detail.'},
+    {title: 'Hello3', content: 'Here comes description in detail.'},
+    {title: 'Hello4', content: 'Here comes description in detail.'},
+    {title: 'Hello5', content: 'Here comes description in detail.'},
+    {title: 'Hello6', content: 'Here comes description in detail.'}
+  ]
+
   const getLocalItems = () => {
     let data = localStorage.getItem('posts');
 
     if(data){
       return JSON.parse(data);
     }else{
-      return [];
+      return defaultData;
     }
   }
 
   const [posts] = useState(getLocalItems);
+
+  //posts에 초기 데이터값이 담기자마자 localStorage에도 데이터 저장
+  useEffect(()=>{
+    localStorage.setItem('posts', JSON.stringify(posts));
+  },[]);
 
   return (
     <section id='news'>
@@ -27,7 +42,7 @@ export default function News(){
                   <p>{post.content}</p>
                 </li>
               )
-            }            
+            }
           })}
         </ul>
       </div>
