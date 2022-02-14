203d2eb01e0383a6062c3df8a70f122b24b9ef5c
tubi55 <tubi55@nate.com>
2022-02-14 14:56:19 +0900

    paginateion, navigation추가

--- src/components/main/Visual.js
+++ src/components/main/Visual.js
@@ -1,13 +1,19 @@
+import { Navigation, Pagination } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 export default function Visual(){ 
   return (
     <figure className='myScroll'>
       <Swiper
+      modules={[Navigation, Pagination]}
         spaceBetween={0}
         slidesPerView={'auto'}   
-        loop   
+        loop  
+        navigation 
+        pagination={{clickable:true}}
       >
         <SwiperSlide>Slide 1</SwiperSlide>
         <SwiperSlide>Slide 2</SwiperSlide>
