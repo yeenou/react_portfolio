70757fadefba32bddb61ae0316211d584425cbe5
tubi55 <tubi55@nate.com>
2022-02-14 11:34:47 +0900

    Btns 자식 컴포넌트에서 부모컴포넌트의 index state값 변경

--- src/components/main/Btns.js
+++ src/components/main/Btns.js
@@ -1,10 +1,10 @@
-export default function Btns(){
+export default function Btns(props){
   return (
     <ul id='btns'>
-      <li className='on'></li>
-      <li></li>
-      <li></li>
-      <li></li>
+      <li className='on' onClick={()=>props.getIndex(0)}></li>
+      <li onClick={()=>props.getIndex(1)}></li>
+      <li onClick={()=>props.getIndex(2)}></li>
+      <li onClick={()=>props.getIndex(3)}></li>
     </ul>
   )
 }
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -5,33 +5,35 @@ import News from './News';
 import Info from './Info';
 import Btns from './Btns';
 import Anime from '../../class/anime.js';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
-export default function Main(){
-  //mainWrap을 참조할 main변수 추가
-  const main = useRef(null);
-  //세로 위치값을 참조할 pos변수 추가
+export default function Main(){ 
+  const main = useRef(null);  
   const pos = useRef([]);
+  const [index, setIndex] = useState(0);
 
-  //참조된 mainWrap에서 .myScroll요소를 모두 찾은뒤 세로 위치값을 pos(ref)로 옮겨담을 함수 정의
+  //자식 컴포넌트에 순서값을 변경하는 함수를 wrappging함수로 정의
+  const getIndex = index => {
+    setIndex(index);
+  }
+
   const getPos = () => {
     const secs = main.current.querySelectorAll('.myScroll');
     let arr = [];
     for(let sec of secs) arr.push(sec.offsetTop);
-    pos.current = arr;
-    console.log(pos.current);
+    pos.current = arr; 
   }
 
-  //컴포넌트 생성시
-  useEffect(()=>{
-    //세로 위치값 구함
-    getPos();
-
-    //브라우저가 리사이즈 될때마다 세로위치값 구함
-    window.addEventListener('resize', getPos);
-    //해당 컴포넌트가 사라질때 window전역에 연결된 getPos함수 제거
+  useEffect(()=>{   
+    getPos();   
+    window.addEventListener('resize', getPos);   
     return ()=> window.removeEventListener('resize', getPos);
   },[]);
+
+  //index값이 변경될때마다 실행될 useEffect추가
+  useEffect(()=>{
+    console.log(index);
+  },[index])
 
   return (
     <div id='mainWrap' ref={main}>
@@ -40,7 +42,8 @@ export default function Main(){
       <Intro />
       <News />
       <Info />
-      <Btns />
+      {/* Btns 자식 컴포넌트에 getIndex함수를 prop으로 전달 */}
+      <Btns getIndex={getIndex} />
     </div>
   )
 }
