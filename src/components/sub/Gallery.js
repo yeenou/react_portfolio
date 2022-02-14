5669e057a225649ccedf09435d33ba2d51f9bcc0
tubi55 <tubi55@nate.com>
2022-02-14 16:38:43 +0900

    갤러리 데이터 호출시 기존 갤러리 사라지고 새로운 갤러리 렌더링되는 모션적용

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -9,22 +9,24 @@ export default function Gallery(){
   const [index, setIndex] = useState(0);
   const api_key = '89aae050d1d8c006bdb5bf866029199d';
   const method1 = 'flickr.interestingness.getList';
+  const method2 = 'flickr.photos.search'
   const num = 20;
-  const url = `https://www.flickr.com/services/rest/?method=${method1}&per_page=${num}&api_key=${api_key}&format=json&nojsoncallback=1`;
+  const url1 = `https://www.flickr.com/services/rest/?method=${method1}&per_page=${num}&api_key=${api_key}&format=json&nojsoncallback=1`;
+  const url2 = `https://www.flickr.com/services/rest/?method=${method2}&per_page=${num}&api_key=${api_key}&format=json&nojsoncallback=1&tags=ocean`;
 
   //promise객체를 리턴하는 함수를 wrapping함수로 감싸주고 앞쪽에  async를 붙임
   //그안쪽에 axios메서드 앞쪽에 await를 붙이면 그다음에 나오는 코드는 무조건 axios가 끝난 이후에 동기적으로 실행됨
-  const getFlickr = async ()=>{
+  const getFlickr = async url =>{
     await axios.get(url).then(json=>{   
       setItems(json.data.photos.photo);
     })
     frame.current.classList.add('on');
   }
 
+
   useEffect(()=>{
     main.current.classList.add('on');
-
-    getFlickr();
+    getFlickr(url1);
   },[]);
 
   return (
@@ -33,7 +35,16 @@ export default function Gallery(){
       <figure></figure>
       
       <div className="inner">
-        <h1>Gallery</h1>
+        <h1 onClick={()=>{
+          frame.current.classList.remove('on');
+          getFlickr(url1);
+        }}>Gallery</h1>
+
+        <button onClick={()=>{
+          frame.current.classList.remove('on');
+          getFlickr(url2);
+        }}>ocean 갤러리 보기</button>
+        
         <section ref={frame}>
           {items.map((item,idx)=>{
             return (
