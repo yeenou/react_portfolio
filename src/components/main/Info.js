37615dc7c625c7b91413f0dfd60d75cb7e050858
tubi55 <tubi55@nate.com>
2022-02-15 15:40:27 +0900

    기본 swiper 슬라이드에 동적 데이터 출력

--- src/components/main/Info.js
+++ src/components/main/Info.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
-import { Navigation, Pagination, EffectCoverflow } from "swiper";
+import { Navigation, Pagination, EffectCoverflow, Virtual } from "swiper";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { useSelector, useDispatch } from 'react-redux';
 import { setYoutube } from '../../redux/actions';
 import 'swiper/css';
+
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import "swiper/css/effect-coverflow";
+import "swiper/css/effect-coverflow"; 
 import { useEffect } from 'react';
 
 export default function Info(){
-  //useSelector로 youtubeReducer에 있는 초기 빈배열을 가져옴
   const vidData = useSelector(state=>state.youtubeReducer.youtube);
   const dispatch = useDispatch();
 
@@ -19,64 +19,29 @@ export default function Info(){
   const num = 10;
   const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${api_key}&playlistId=${play_list}&maxResults=${num}`;
 
-  //fetchYoutube함수가 호출되면 axios로 받아온 youte data를 dispatch로 reducer에 전달
   const fetchYoutube = async () => {
     await axios.get(url).then(json=>{
-      console.log(json)
-      dispatch(setYoutube(json.data.items));
-    });
-    
+      console.log(json.data.items);
+      dispatch(setYoutube(json.data.items));     
+    });    
   }
 
   useEffect(()=>{
-    fetchYoutube();
-    console.log(vidData);
+    fetchYoutube();   
   },[]);
 
-
   return (
-    <section id='info' className='myScroll'>
-      <div className="inner">
+    <section id='info'>
+      <div className='inner'>
         <h1>Information</h1>
-
-        {/* {vidData.map((vid,idx)=><p key={idx}>{vid.snippet.title}</p>)} */}
-
-        <div className="wrap">          
-          <Swiper
-            modules={[Navigation, Pagination, EffectCoverflow]}
-            spaceBetween={250}
-            slidesPerView={'auto'}   
-            loop  
-            navigation 
-            centeredSlides={true}
-            grabCursor={true}
-            pagination={{clickable:true}}
-            effect= {'coverflow'}
-            coverflowEffect={{
-              rotate: 50,
-              stretch: 0,
-              depth: 200,
-              modifier: 1,
-              slideShadows: true,
-            }}
-          >
-            <SwiperSlide>
-              {vidData[0].snippet.title}
+        <Swiper modules={[Virtual]} spaceBetween={50} slidesPerView={3} virtual>
+          {vidData.map((vid, index) => (
+            <SwiperSlide key={index} virtualIndex={index}>
+              <img src={vid.snippet.thumbnails.medium.url} />
+              {vid.snippet.title}
             </SwiperSlide>
-            <SwiperSlide>
-              {vidData[1].snippet.title}
-            </SwiperSlide>
-            <SwiperSlide>
-              {vidData[2].snippet.title}
-            </SwiperSlide>
-            <SwiperSlide>
-              {vidData[3].snippet.title}
-            </SwiperSlide>
-            <SwiperSlide>
-              {vidData[4].snippet.title}
-            </SwiperSlide>
-          </Swiper>
-        </div>  
+          ))}
+        </Swiper>
       </div>
     </section>
   )
