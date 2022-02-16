b2bac1f995870d9bbf041e3f0ce2a2ecb0cefcc7
tubi55 <tubi55@nate.com>
2022-02-16 12:15:56 +0900

    기본 토글메뉴 설정

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,26 +1,40 @@
 import {NavLink} from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react';
 
-export default function Header(props){
-  const active = {color: 'aqua'}
+export default function Header(props){  
+  const [isOn, setIsOn] = useState(false);
+  const toggleNav = () => setIsOn(!isOn);
 
   return (
-    <header className={props.type}>
-      <div className="inner">
-        <h1><NavLink exact to='/'>LOGO</NavLink></h1>
-
-        <ul id="gnb">
-          <li><NavLink activeStyle={active} to='/department'>Department</NavLink></li>
-          <li><NavLink activeStyle={active} to='/community'>Community</NavLink></li>
-          <li><NavLink activeStyle={active} to='/gallery'>Gallery</NavLink></li>
-          <li><NavLink activeStyle={active} to='/youtube'>Youtube</NavLink></li>
-          <li><NavLink activeStyle={active} to='/location'>Location</NavLink></li>
-          <li><NavLink activeStyle={active} to='/join'>Join</NavLink></li>
-        </ul>
-
-        <FontAwesomeIcon icon={faBars} />
-      </div>
-    </header>
+    <>
+      <header className={props.type}>
+        <div className="inner">
+          <h1><NavLink exact to='/'>LOGO</NavLink></h1>
+          <Gnb />
+          <FontAwesomeIcon icon={faBars} onClick={toggleNav} />
+        </div>
+      </header>
+      
+      <nav className={isOn ? 'on' : ''}>
+        <Gnb />
+      </nav>
+    </>
   )
+}
+
+function Gnb(){
+  const active = {color: 'aqua'}
+  return (
+    <ul id="gnb">
+      <li><NavLink activeStyle={active} to='/department'>Department</NavLink></li>
+      <li><NavLink activeStyle={active} to='/community'>Community</NavLink></li>
+      <li><NavLink activeStyle={active} to='/gallery'>Gallery</NavLink></li>
+      <li><NavLink activeStyle={active} to='/youtube'>Youtube</NavLink></li>
+      <li><NavLink activeStyle={active} to='/location'>Location</NavLink></li>
+      <li><NavLink activeStyle={active} to='/join'>Join</NavLink></li>
+    </ul>
+  )
+  
 }
