ed7e3fe357d3b669a16e467a1049f975462c540c
tubi55 <tubi55@nate.com>
2022-02-16 11:42:47 +0900

    redux-persist로 redux상태관리값 localstorage에 저장

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {HashRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
-import store from './redux/store';
+import store, {Persistor} from './redux/store';
+import {PersistGate} from 'redux-persist/integration/react';
 
 ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={Persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </HashRouter>    
   </React.StrictMode>,
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,16 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-//store를 생성시 reducer로 전달된 값을 적용;
-const store = createStore(reducers);
+const persistConfig = {
+  key: 'main-root',
+  storage
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+const store = createStore(persistedReducer, applyMiddleware());
+const Persistor = persistStore(store);
+
+export {Persistor};
 export default store;
