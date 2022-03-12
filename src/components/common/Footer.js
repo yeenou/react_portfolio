735cd3401ab2746d374c345b546196b2fa7df8bd
yeenou <cye0328@naver.com>
2022-03-12 14:04:49 +0900

    footer

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -2,7 +2,30 @@ export default function Footer(){
   return (
     <footer>
       <div className="inner">
-        <p>2022 DCODELAB &copy; ALL RIGHTS RESERVED.</p>
+
+        {/* <ul>
+          <li>+7 (212) 1234 567</li>
+          <li>+7 (212) 1234 567</li>
+          <li>hello@gmail.com</li>
+          <li>USA New York 57 Qulgley Bridge</li>
+        </ul>
+
+        <article className="send">
+            <input type="text" placeholder='Name' />
+            <input type="text" placeholder='Phone' />
+            <input type="text" placeholder='E-mail' />
+            <input type="text" placeholder='Company' />
+            <input type="text" placeholder='Message' />
+            <button>SEND</button>
+          </article> */}
+
+
+        <div className="wrap">
+          <h1>LOGO <span>.</span></h1>
+          <p>PORTFOLIO</p>
+          <p>2022 LOGO &copy; ALL RIGHTS RESERVED.</p>
+        </div>
+        
       </div>
     </footer>
   )
